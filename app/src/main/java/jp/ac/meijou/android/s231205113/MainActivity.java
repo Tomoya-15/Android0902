60c7590b3df9f65b3d1b267870792c726eb4e563
tomoya taniguchi <231205113@ccmailg.meijo-u.ac.jp>
2024-09-03 15:42:19 +0900

    ボタンを作成した

--- app/src/main/java/jp/ac/meijou/android/s231205113/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205113/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s231205113;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import jp.ac.meijou.android.s231205113.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +24,58 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.text.setText(R.string.text2);
+        prefDataStore = PrefDataStore.getInstance(this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        var train = ContextCompat.getDrawable(this, R.drawable.baseline_android_24);
-        binding.imageView.setImageDrawable(train);
-}
+
+        binding.button.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(R.string.text2);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // テキストが更新される直前に呼ばれる
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // 文字を1つ入力されたときに呼ばれる
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // テキストが更新されたあとに呼ばれる
+                binding.text.setText(editable.toString());
+            }
+        });
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
+        binding.deleteButton.setOnClickListener(view -> {
+            prefDataStore.setString("name", "");
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("name")
+                .ifPresent(name -> binding.text.setText(name)); // .ifPresent()は「getString("name")の結果がnullでないときに，引数の処理を実行する」というOptionalのメソッド
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        var text = binding.editTextText.getText().toString();
+        prefDataStore.setString("name", text);
+    }
 }
