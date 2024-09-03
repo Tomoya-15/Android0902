453819efdcc01d7983ff8792613b2dea6ce79442
tomoya taniguchi <231205113@ccmailg.meijo-u.ac.jp>
2024-09-03 12:18:22 +0900

    画像を追加した．bindingを使ってコードから画像とテキストを変更することができるようにした．

--- app/src/main/java/jp/ac/meijou/android/s231205113/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205113/MainActivity.java
@@ -4,21 +4,30 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s231205113.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.text.setText(R.string.text2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        var andorid = ContextCompat.getDrawable(this, R.drawable.baseline_android_24);
+        binding.imageView.setImageDrawable(andorid);
     }
 }
