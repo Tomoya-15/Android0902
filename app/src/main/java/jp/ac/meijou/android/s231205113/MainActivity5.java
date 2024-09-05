114a2a3a4bfe7dfd66dba2514128e0b4c4c75eea
tomoya taniguchi <231205113@ccmailg.meijo-u.ac.jp>
2024-09-05 14:39:52 +0900

    ipアドレスと通信できるかの判定，webAPIによる画像の表示

---
+++ app/src/main/java/jp/ac/meijou/android/s231205113/MainActivity5.java
@@ -0,0 +1,88 @@
+package jp.ac.meijou.android.s231205113;
+
+import android.app.DownloadManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
+import jp.ac.meijou.android.s231205113.databinding.ActivityMain5Binding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
+    private ActivityMain5Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain5Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.progressBar.setVisibility(View.INVISIBLE);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.getButton.setOnClickListener(view -> {
+            var text = binding.edittext.getText().toString();
+
+            // textパラメータをつけたURLの作成
+            var url = Uri.parse("https://placehold.jp/3d4070/ffffff/500x500.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+
+            getImage(url);
+        });
+    }
+
+    private void getImage(String url) {
+        // リクエスト先に画像URLを指定
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+
+        // 非同期通信でリクエスト
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // InputStreamをBitmapに変換
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                runOnUiThread(() -> {
+                    binding.progressBar.setVisibility(View.VISIBLE);
+                    binding.image.setImageBitmap(bitmap);
+                });
+            }
+        });
+    }
+}
